709935e98ea29ff888d33195e418843554842299
Yacine abd <tunisiemontana@live.fr>
2020-03-25 15:02:16 +0100

    settings

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const mysql =  require("mysql");
 const app = express();
-const app.listen(3000, ()=>
+const settings = require("./settings.json");
+const sqlConfig = settings.sqlConfig;
+
+app.listen( 3000, () =>
 {
     console.log("Serveur stated");
 });
 app.get('/',(req,res)=>{
+    const sqlConnection= mysql.createConnection(sqlConfig);
+    sqlConnection.query("SELECT id, prenom, nom , email, birthdate FROM node_users", (error,result)=> {
+        if(error){
+            console.log("ERROR:",error.code);       
+        }
+        else{
+            console.log(" RESULT:",result);
+        }sqlConnection.end();
+    });
+
     res.send({
         id:1,
         email:"yacineabd@outlook.fr",
         prenom:"yacine",
         nom:"abd",
-        birthdate: new Date(1995,9,22)
+        birthdate: new Date(1995,7,22)
     });
 });
